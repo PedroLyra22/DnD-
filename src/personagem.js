481a119ed844c92b22e49ef3e138cbb761e8f283
PedroLyra22 <pedrocalezanelyra@gmail.com>
2024-05-13 15:39:43 -0300

    Todos os níveis de xp implementados

--- src/personagem.js
+++ src/personagem.js
@@ -18,12 +18,47 @@ function calculaNivel(xp){
         case(xp < 23000):
             return 6;
             break; 
-        //...
-
-        default:
-            console.log("entrei default");
-            return Math.floor((Number(campo.value) + 100) / 100);
-
+        case(xp < 34000):
+            return 7;
+            break;
+        case(xp < 48000):
+            return 8;
+            break;
+        case(xp < 64000):
+            return 9;
+            break;
+        case(xp < 85000):
+            return 10;
+            break;
+        case(xp < 100000):
+            return 11;
+            break;
+        case(xp < 120000):
+            return 12;
+            break;
+        case(xp < 140000):
+            return 13;
+            break;
+        case(xp < 165000):
+            return 14;
+            break;
+        case(xp < 195000):
+            return 15;
+            break;
+        case(xp < 225000):
+            return 16;
+            break;
+        case(xp < 265000):
+            return 17;
+            break;
+        case(xp < 305000):
+            return 18;
+            break;
+        case(xp < 355000):
+            return 19;
+            break;
+        case(xp >= 355000):
+            return 20;
             break;
     }
 }
