88f6d7c087ca63f5695e821027fff506be5939b3
PedroLyra22 <pedrocalezanelyra@gmail.com>
2024-06-26 09:36:34 -0300

    add atributos mais simples em persnagem/ alteração de xp por nvl

--- src/personagem.js
+++ src/personagem.js
@@ -7,9 +7,7 @@ class Personagem {
         this.classe = objPersonagem.chaClass;
         this.antecedente = objPersonagem.background;
         this.alinhamento = objPersonagem.allingment;
-
-        this.xp = objPersonagem.xp;
-        this.nivel = 1;
+        this.nivel = objPersonagem.nvl;
 
     }
 
@@ -19,70 +17,6 @@ class Personagem {
 
 }
 
-function calculaNivel(xp){
-    switch (true) {
-        case(xp < 300):
-            return 1;
-            break;
-        case(xp < 900):
-            return 2;
-            break;
-        case(xp < 2700):
-            return 3;
-            break;    
-        case(xp < 6500):
-            return 4;
-            break;
-        case(xp < 14000):
-            return 5;
-            break;
-        case(xp < 23000):
-            return 6;
-            break; 
-        case(xp < 34000):
-            return 7;
-            break;
-        case(xp < 48000):
-            return 8;
-            break;
-        case(xp < 64000):
-            return 9;
-            break;
-        case(xp < 85000):
-            return 10;
-            break;
-        case(xp < 100000):
-            return 11;
-            break;
-        case(xp < 120000):
-            return 12;
-            break;
-        case(xp < 140000):
-            return 13;
-            break;
-        case(xp < 165000):
-            return 14;
-            break;
-        case(xp < 195000):
-            return 15;
-            break;
-        case(xp < 225000):
-            return 16;
-            break;
-        case(xp < 265000):
-            return 17;
-            break;
-        case(xp < 305000):
-            return 18;
-            break;
-        case(xp < 355000):
-            return 19;
-            break;
-        case(xp >= 355000):
-            return 20;
-            break;
-    }
-}
 
 function calculaCustoAtributos(atr){ 
     var custo = [-20, -16, -12, -9, -6, -4, -2, -1, 0, 1, 2, 3, 4, 5, 7, 9, 12, 15, 19];
@@ -91,13 +25,80 @@ function calculaCustoAtributos(atr){
 
 function somaCustosAtribustos(Afor, Adex, Acon, Aint, Asab, Acar){
     return calculaCustoAtributos(Number(Afor))+
-           calculaCustoAtributos(Number(Adex))+
+    calculaCustoAtributos(Number(Adex))+
            calculaCustoAtributos(Number(Acon))+
            calculaCustoAtributos(Number(Aint))+
            calculaCustoAtributos(Number(Asab))+
            calculaCustoAtributos(Number(Acar))
-}
+        }
 
 function calculaModificador(atr){    
     return Math.floor((atr - 10)/2);
+}
+
+
+//Função Esquecida :(
+function calculaNivel(xp){
+            switch (true) {
+                case(xp < 300):
+                    return 1;
+                    break;
+                case(xp < 900):
+                    return 2;
+                    break;
+                case(xp < 2700):
+                    return 3;
+                    break;    
+                case(xp < 6500):
+                    return 4;
+                    break;
+                case(xp < 14000):
+                    return 5;
+                    break;
+                case(xp < 23000):
+                    return 6;
+                    break; 
+                case(xp < 34000):
+                    return 7;
+                    break;
+                case(xp < 48000):
+                    return 8;
+                    break;
+                case(xp < 64000):
+                    return 9;
+                    break;
+                case(xp < 85000):
+                    return 10;
+                    break;
+                case(xp < 100000):
+                    return 11;
+                    break;
+                case(xp < 120000):
+                    return 12;
+                    break;
+                case(xp < 140000):
+                    return 13;
+                    break;
+                case(xp < 165000):
+                    return 14;
+                    break;
+                case(xp < 195000):
+                    return 15;
+                    break;
+                case(xp < 225000):
+                    return 16;
+                    break;
+                case(xp < 265000):
+                    return 17;
+                    break;
+                case(xp < 305000):
+                    return 18;
+                    break;
+                case(xp < 355000):
+                    return 19;
+                    break;
+                case(xp >= 355000):
+                    return 20;
+                    break;
+            }
 }
--- src/script.js
+++ src/script.js
@@ -1,227 +1,209 @@
-var form = document.getElementById("formFicha")
-var divFicha = document.getElementById("ficha")
-var custo = document.getElementById("custo")
+var form = document.getElementById("formFicha");
+var divFicha = document.getElementById("ficha");
+var custo = document.getElementById("custo");
 
 const testando = true;
-if (testando){
-    form.chaName.value = "Valente"
-    form.playName.value = "Pedro"
-    form.race.value = "Tabaxi"
-    form.chaClass.value = "Ladino"
-    form.background.value = "Criminoso"
-    form.allingment.value = "Caótico e Bom"
+if (testando) {
+  form.chaName.value = "Valente";
+  form.playName.value = "Pedro";
+  form.race.value = "Tabaxi";
+  form.chaClass.value = "Ladino";
+  form.background.value = "Criminoso";
+  form.allingment.value = "Caótico e Bom";
+  form.nvl.value = "3";
 }
 
-function imprimeCustoAtributo(){
-    const VatrF = document.getElementById("força").value
-    const VatrD = document.getElementById("destreza").value
-    const VatrCo = document.getElementById("constituição").value
-    const VatrI = document.getElementById("inteligência").value
-    const VatrS = document.getElementById("sabedoria").value
-    const VatrCa = document.getElementById("carisma").value
-    custo.textContent = `${somaCustosAtribustos(VatrF, VatrD, VatrCo, 
-        VatrI, VatrS, VatrCa)} / 27`
+function imprimeCustoAtributo() {
+  const VatrF = document.getElementById("força").value;
+  const VatrD = document.getElementById("destreza").value;
+  const VatrCo = document.getElementById("constituição").value;
+  const VatrI = document.getElementById("inteligência").value;
+  const VatrS = document.getElementById("sabedoria").value;
+  const VatrCa = document.getElementById("carisma").value;
+  custo.textContent = `${somaCustosAtribustos(
+    VatrF,
+    VatrD,
+    VatrCo,
+    VatrI,
+    VatrS,
+    VatrCa
+  )} / 27`;
 }
 
 imprimeCustoAtributo();
 
 function criarFicha(event) {
-    event.preventDefault();
-    const campos = event.target;
-    const camposPersonagem = document.getElementsByClassName("campoPersonagem");
-    const objPersonagem = {};
-    
-for (let i=0; i< camposPersonagem.length; i++){
+  event.preventDefault();
+  const campos = event.target;
+  const camposPersonagem = document.getElementsByClassName("campoPersonagem");
+  const objPersonagem = {};
+
+  for (let i = 0; i < camposPersonagem.length; i++) {
     objPersonagem[camposPersonagem[i].id] = camposPersonagem[i].value;
-}
+  }
 
-const personagem = new Personagem(objPersonagem);
-personagem.imprime();
+  const personagem = new Personagem(objPersonagem);
 
-    for(campo of campos){
-        
+  const NomePersonagem = document.createElement("h1");
+  NomePersonagem.textContent = `Nome do Personagem: ${personagem.nome}`;
+  divFicha.appendChild(NomePersonagem);
 
-        if(campo.id === "chaName"){           
-            const NomePersonagem = document.createElement('h1');
-            NomePersonagem.textContent = `Nome do Personagem: ${campo.value}`;
-            divFicha.appendChild(NomePersonagem);
-        }
+  const NomeJogador = document.createElement("h3");
+  NomeJogador.textContent = `Nome do Jogador: ${personagem.jogador}`;
+  divFicha.appendChild(NomeJogador);
 
-        if(campo.id === "playName"){           
-            const NomeJogador = document.createElement('h3');
-            NomeJogador.textContent = `Nome do Jogador: ${campo.value}`;
-            divFicha.appendChild(NomeJogador);
-        }
+  const Raca = document.createElement("h3");
+  Raca.textContent = `Linhagem: ${personagem.raça}`;
+  divFicha.appendChild(Raca);
 
-        if(campo.id === "race"){           
-            const Raca = document.createElement('h3');
-            Raca.textContent = `Linhagem: ${campo[campo.selectedIndex].value}`;
-            divFicha.appendChild(Raca);
-        }
+  const Classe = document.createElement("h3");
+  Classe.textContent = `Classe: ${personagem.classe}`;
+  divFicha.appendChild(Classe);
 
-        if(campo.id === "chaClass"){           
-            const Classe = document.createElement('h3');
-            Classe.textContent = `Classe: ${campo[campo.selectedIndex].value}`;
-            divFicha.appendChild(Classe);
-        }
+  const Antecedente = document.createElement("h3");
+  Antecedente.textContent = `Antecedente: ${personagem.antecedente}`;
+  divFicha.appendChild(Antecedente);
 
-        if(campo.id === "background"){           
-            const Antecedente = document.createElement('h3');
-            Antecedente.textContent = `Antecedente: ${campo[campo.selectedIndex].value}`;
-            divFicha.appendChild(Antecedente);
-        }
+  const Alinhamento = document.createElement("h3");
+  Alinhamento.textContent = `Alinhamento: ${personagem.alinhamento}`;
+  divFicha.appendChild(Alinhamento);
 
-        if(campo.id === "allingment"){           
-            const Alinhamento = document.createElement('h3');
-            Alinhamento.textContent = `Alinhamento: ${campo[campo.selectedIndex].value}`;
-            divFicha.appendChild(Alinhamento);
-        }
+  const Nivel = document.createElement("h3");
+  Nivel.textContent = `Nível: ${personagem.nivel}`;
+  divFicha.appendChild(Nivel);
 
-        if(campo.id === "exp"){           
-            const Experiencia = document.createElement('h3');
-            const Nivel = document.createElement('h3')
-            const xp = Number(campo.value);
+  //---------------------------ATRIBUTOS-------------------------------------
+  for (campo of campos) {
+    if (campo.id === "força") {
+      const CaixaFor = document.createElement("div");
+      const QtdFor = document.createElement("p");
+      const Modificador = document.createElement("h2");
+      const Titulo = document.createElement("h4");
 
-            Experiencia.textContent = `Experiencia: ${campo.value}`;
-            Nivel.textContent = `Nível: ${calculaNivel(xp)}`;
+      CaixaFor.id = "caixaFor";
+      QtdFor.id = "qtdFor";
+      Modificador.id = "modFor";
+      Titulo.id = "titFor";
 
-            divFicha.appendChild(Experiencia);
-            divFicha.appendChild(Nivel);
-        }
+      QtdFor.textContent = campo.valueAsNumber;
+      Modificador.textContent = calculaModificador(campo.valueAsNumber);
+      Titulo.textContent = "FOR";
 
-        if(campo.id === "força"){
-            const CaixaFor = document.createElement('div');
-            const QtdFor = document.createElement('p');
-            const Modificador = document.createElement('h2');
-            const Titulo = document.createElement('h4')
-
-            CaixaFor.id = "caixaFor";
-            QtdFor.id = "qtdFor";
-            Modificador.id = "modFor";
-            Titulo.id = "titFor"
-
-            QtdFor.textContent = campo.valueAsNumber;
-            Modificador.textContent = calculaModificador(campo.valueAsNumber);
-            Titulo.textContent = "FOR";
-
-            CaixaFor.appendChild(QtdFor);
-            CaixaFor.appendChild(Modificador);
-            CaixaFor.appendChild(Titulo);
-            divFicha.appendChild(CaixaFor);
-        }
-
-        if(campo.id === "destreza"){
-            const CaixaDex = document.createElement('div');
-            const QtdDex = document.createElement('p');
-            const Modificador = document.createElement('h2');
-            const Titulo = document.createElement('h4')
-
-            CaixaDex.id = "caixaDex";
-            QtdDex.id = "qtdDex";
-            Modificador.id = "modDex";
-            Titulo.id = "titDex"
-
-            QtdDex.textContent = campo.valueAsNumber;
-            Modificador.textContent = calculaModificador(campo.valueAsNumber);
-            Titulo.textContent = "DEX";
-
-            CaixaDex.appendChild(QtdDex);
-            CaixaDex.appendChild(Modificador);
-            CaixaDex.appendChild(Titulo);
-            divFicha.appendChild(CaixaDex);
-        }
-
-        if(campo.id === "constituição"){
-            const CaixaCon = document.createElement('div');
-            const QtdCon = document.createElement('p');
-            const Modificador = document.createElement('h2');
-            const Titulo = document.createElement('h4')
-
-            CaixaCon.id = "caixaCon";
-            QtdCon.id = "qtdCon";
-            Modificador.id = "modCon";
-            Titulo.id = "titCon"
-
-            QtdCon.textContent = campo.valueAsNumber;
-            Modificador.textContent = calculaModificador(campo.valueAsNumber);
-            Titulo.textContent = "CON";
-
-            CaixaCon.appendChild(QtdCon);
-            CaixaCon.appendChild(Modificador);
-            CaixaCon.appendChild(Titulo);
-            divFicha.appendChild(CaixaCon);
-        }
-
-        if(campo.id === "inteligência"){
-            const CaixaInt = document.createElement('div');
-            const QtdInt = document.createElement('p');
-            const Modificador = document.createElement('h2');
-            const Titulo = document.createElement('h4')
-
-            CaixaInt.id = "caixaInt";
-            QtdInt.id = "qtdInt";
-            Modificador.id = "modInt";
-            Titulo.id = "titInt"
-
-            QtdInt.textContent = campo.valueAsNumber;
-            Modificador.textContent = calculaModificador(campo.valueAsNumber);
-            Titulo.textContent = "INT";
-
-            CaixaInt.appendChild(QtdInt);
-            CaixaInt.appendChild(Modificador);
-            CaixaInt.appendChild(Titulo);
-            divFicha.appendChild(CaixaInt);
-        }
-
-        if(campo.id === "sabedoria"){
-            const CaixaSab = document.createElement('div');
-            const QtdSab = document.createElement('p');
-            const Modificador = document.createElement('h2');
-            const Titulo = document.createElement('h4')
-
-            CaixaSab.id = "caixaSab";
-            QtdSab.id = "qtdSab";
-            Modificador.id = "modSab";
-            Titulo.id = "titSab"
-
-            QtdSab.textContent = campo.valueAsNumber;
-            Modificador.textContent = calculaModificador(campo.valueAsNumber);
-            Titulo.textContent = "SAB";
-
-            CaixaSab.appendChild(QtdSab);
-            CaixaSab.appendChild(Modificador);
-            CaixaSab.appendChild(Titulo);
-            divFicha.appendChild(CaixaSab);
-        }
-
-        if(campo.id === "carisma"){
-            const CaixaCar = document.createElement('div');
-            const QtdCar = document.createElement('p');
-            const Modificador = document.createElement('h2');
-            const Titulo = document.createElement('h4')
-
-            CaixaCar.id = "caixaCar";
-            QtdCar.id = "qtdCar";
-            Modificador.id = "modCar";
-            Titulo.id = "titCar"
-
-            QtdCar.textContent = campo.valueAsNumber;
-            Modificador.textContent = calculaModificador(campo.valueAsNumber);
-            Titulo.textContent = "CHA";
-
-            CaixaCar.appendChild(QtdCar);
-            CaixaCar.appendChild(Modificador);
-            CaixaCar.appendChild(Titulo);
-            divFicha.appendChild(CaixaCar);
-        }
+      CaixaFor.appendChild(QtdFor);
+      CaixaFor.appendChild(Modificador);
+      CaixaFor.appendChild(Titulo);
+      divFicha.appendChild(CaixaFor);
     }
-}
 
+    if (campo.id === "destreza") {
+      const CaixaDex = document.createElement("div");
+      const QtdDex = document.createElement("p");
+      const Modificador = document.createElement("h2");
+      const Titulo = document.createElement("h4");
+
+      CaixaDex.id = "caixaDex";
+      QtdDex.id = "qtdDex";
+      Modificador.id = "modDex";
+      Titulo.id = "titDex";
+
+      QtdDex.textContent = campo.valueAsNumber;
+      Modificador.textContent = calculaModificador(campo.valueAsNumber);
+      Titulo.textContent = "DEX";
+
+      CaixaDex.appendChild(QtdDex);
+      CaixaDex.appendChild(Modificador);
+      CaixaDex.appendChild(Titulo);
+      divFicha.appendChild(CaixaDex);
+    }
+
+    if (campo.id === "constituição") {
+      const CaixaCon = document.createElement("div");
+      const QtdCon = document.createElement("p");
+      const Modificador = document.createElement("h2");
+      const Titulo = document.createElement("h4");
+
+      CaixaCon.id = "caixaCon";
+      QtdCon.id = "qtdCon";
+      Modificador.id = "modCon";
+      Titulo.id = "titCon";
+
+      QtdCon.textContent = campo.valueAsNumber;
+      Modificador.textContent = calculaModificador(campo.valueAsNumber);
+      Titulo.textContent = "CON";
+
+      CaixaCon.appendChild(QtdCon);
+      CaixaCon.appendChild(Modificador);
+      CaixaCon.appendChild(Titulo);
+      divFicha.appendChild(CaixaCon);
+    }
+
+    if (campo.id === "inteligência") {
+      const CaixaInt = document.createElement("div");
+      const QtdInt = document.createElement("p");
+      const Modificador = document.createElement("h2");
+      const Titulo = document.createElement("h4");
+
+      CaixaInt.id = "caixaInt";
+      QtdInt.id = "qtdInt";
+      Modificador.id = "modInt";
+      Titulo.id = "titInt";
+
+      QtdInt.textContent = campo.valueAsNumber;
+      Modificador.textContent = calculaModificador(campo.valueAsNumber);
+      Titulo.textContent = "INT";
+
+      CaixaInt.appendChild(QtdInt);
+      CaixaInt.appendChild(Modificador);
+      CaixaInt.appendChild(Titulo);
+      divFicha.appendChild(CaixaInt);
+    }
+
+    if (campo.id === "sabedoria") {
+      const CaixaSab = document.createElement("div");
+      const QtdSab = document.createElement("p");
+      const Modificador = document.createElement("h2");
+      const Titulo = document.createElement("h4");
+
+      CaixaSab.id = "caixaSab";
+      QtdSab.id = "qtdSab";
+      Modificador.id = "modSab";
+      Titulo.id = "titSab";
+
+      QtdSab.textContent = campo.valueAsNumber;
+      Modificador.textContent = calculaModificador(campo.valueAsNumber);
+      Titulo.textContent = "SAB";
+
+      CaixaSab.appendChild(QtdSab);
+      CaixaSab.appendChild(Modificador);
+      CaixaSab.appendChild(Titulo);
+      divFicha.appendChild(CaixaSab);
+    }
+
+    if (campo.id === "carisma") {
+      const CaixaCar = document.createElement("div");
+      const QtdCar = document.createElement("p");
+      const Modificador = document.createElement("h2");
+      const Titulo = document.createElement("h4");
+
+      CaixaCar.id = "caixaCar";
+      QtdCar.id = "qtdCar";
+      Modificador.id = "modCar";
+      Titulo.id = "titCar";
+
+      QtdCar.textContent = campo.valueAsNumber;
+      Modificador.textContent = calculaModificador(campo.valueAsNumber);
+      Titulo.textContent = "CHA";
+
+      CaixaCar.appendChild(QtdCar);
+      CaixaCar.appendChild(Modificador);
+      CaixaCar.appendChild(Titulo);
+      divFicha.appendChild(CaixaCar);
+    }
+  }
+}
 
 const atributos = document.getElementsByClassName("atr");
-for(let i=0; i < atributos.length; i++){
-    atributos[i].addEventListener("input", imprimeCustoAtributo);
+for (let i = 0; i < atributos.length; i++) {
+  atributos[i].addEventListener("input", imprimeCustoAtributo);
 }
 form.addEventListener("submit", criarFicha);
-
-
